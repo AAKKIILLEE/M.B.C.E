7e97aaa72c4a55b9c5a3ecf4270ee8311a860b0f
AAKKIILLEE <130297981+AAKKIILLEE@users.noreply.github.com>
2025-11-18 17:04:01 +0100

    Format and clean up Program.cs

--- Most.Basic.Calculator.Ever/Most.Basic.Calculator.Ever/Program.cs
+++ Most.Basic.Calculator.Ever/Most.Basic.Calculator.Ever/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.NetworkInformation;
@@ -12,31 +12,32 @@ namespace Most.Basic.Calculator.Ever
         static void Main(string[] args)
         {
 
-           
 
-            double r=0;
+
+            double r = 0;
 
 
             Console.Write("enter the first digit: ");
-            double a=Convert.ToDouble(Console.ReadLine());
+            double a = Convert.ToDouble(Console.ReadLine());
 
             Console.Write("enter the operation: ");
-            string c=Console.ReadLine();
+            string c = Console.ReadLine();
 
             Console.Write("enter the second digit: ");
             double b = Convert.ToDouble(Console.ReadLine());
 
 
-            switch (c){
+            switch (c)
+            {
 
-                case "+": r=  a + b; Console.WriteLine(a + "+" + b + "=" + r); break;
+                case "+": r = a + b; Console.WriteLine(a + "+" + b + "=" + r); break;
                 case "-": r = a - b; Console.WriteLine(a + "-" + b + "=" + r); break;
                 case "*": r = a * b; Console.WriteLine(a + "*" + b + "=" + r); break;
                 case "/": r = a / b; Console.WriteLine(a + "/" + b + "=" + r); break;
-                default:  Console.WriteLine("something went wrong");
-              }
+                default: Console.WriteLine("something went wrong"); break;
+            }
 
-            
+
 
 
 

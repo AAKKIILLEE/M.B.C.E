26452db9d461c56bd90d2764ac12c8b9312bd82f
AAKKIILLEE <130297981+AAKKIILLEE@users.noreply.github.com>
2025-11-18 16:12:55 +0100

    Add files via upload

---
+++ Most.Basic.Calculator.Ever/Most.Basic.Calculator.Ever/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Most.Basic.Calculator.Ever
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+           
+
+            double r=0;
+
+
+            Console.Write("enter the first digit: ");
+            double a=Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("enter the operation: ");
+            string c=Console.ReadLine();
+
+            Console.Write("enter the second digit: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+
+            switch (c){
+
+                case "+": r=  a + b; Console.WriteLine(a + "+" + b + "=" + r); break;
+                case "-": r = a - b; Console.WriteLine(a + "-" + b + "=" + r); break;
+                case "*": r = a * b; Console.WriteLine(a + "*" + b + "=" + r); break;
+                case "/": r = a / b; Console.WriteLine(a + "/" + b + "=" + r); break;
+              }
+
+            
+
+
+
+
+
+
+        }
+    }
+}
